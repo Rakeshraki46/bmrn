fe9ee732f5cfbc8a295fdd5ec0b2242402faa6d9
rakeshraki46 <rakesh.joruka9999@gmail.com>
2025-07-21 16:56:51 +0530

    from devops through v

--- output/bmrn/1.2.0/bmrn.psm1
+++ output/bmrn/1.2.0/bmrn.psm1
@@ -1,6 +1,4 @@
 function create {
-    write-host "from devops through vs new line of code totally changed"
-      write-host "from devops through vs new line of code totally changed"
-        write-host "from devops through vs new line of code totally changed"
-        vctl.exev
+    write-host "from devops through vs code "
 }
+    
