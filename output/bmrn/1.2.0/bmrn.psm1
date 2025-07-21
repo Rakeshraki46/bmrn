cd1662cb2f9b07642bfeabf5ca132acb357153eb
rakeshraki46 <rakesh.joruka9999@gmail.com>
2025-07-21 15:54:15 +0530

    vs commit

--- output/bmrn/1.2.0/bmrn.psm1
+++ output/bmrn/1.2.0/bmrn.psm1
@@ -1,3 +1,6 @@
 function create {
     write-host "from devops through vs new line of code totally changed"
+      write-host "from devops through vs new line of code totally changed"
+        write-host "from devops through vs new line of code totally changed"
+        vctl.exev
 }
