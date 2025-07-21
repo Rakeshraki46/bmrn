17665bb5990cf287b2e336cb276edfd66672239f
rakeshraki46 <rakesh.joruka9999@gmail.com>
2025-07-21 19:38:15 +0530

    pushing code attime to github and devops

--- output/bmrn/1.2.0/bmrn.psm1
+++ output/bmrn/1.2.0/bmrn.psm1
@@ -9,4 +9,11 @@ function create {
     write-host "from devops through vs new line of code totally changed"
     write-host "from devops through vs new line of code totally changed"
     write-host "from devops through vs new line of code totally changed"
+
+
+    write-host "pushing code in one command to github as well devops"
+      write-host "pushing code in one command to github as well devops"
+        write-host "pushing code in one command to github as well devops"
+          write-host "pushing code in one command to github as well devops"
+            write-host "pushing code in one command to github as well devops"
 }
