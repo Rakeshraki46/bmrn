63e979b6cc43dd6ffea090511d2caacd23badf27
Rakesh joruka <Rakeshjoruka@MegThink582.onmicrosoft.com>
2025-07-21 09:18:43 +0000

    This is the new commit from bmrn_from_devops

---
+++ output/bmrn/1.2.0/bmrn.psm1
@@ -0,0 +1,6 @@
+function Create-Folder {
+    New-Item -Path "C:\TestBmrn" -ItemType Directory
+    Write-Host "✅ Folder C:\TestBmrn created successfully from devops !"
+     Write-Host "✅ Folder C:\TestBmrn created successfully from commiting 2nd from devops !"
+     Write-Host "✅ Folder C:\TestBmrn created successfully from commiting 3rd from devops through vs !"
+}
