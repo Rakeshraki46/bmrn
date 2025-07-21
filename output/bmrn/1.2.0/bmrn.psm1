945c196e2ce63d4c1b78276c6b588ba4d4a7804f
rakeshraki46 <rakesh.joruka9999@gmail.com>
2025-07-21 16:59:50 +0530

    from devops through vs

--- output/bmrn/1.2.0/bmrn.psm1
+++ output/bmrn/1.2.0/bmrn.psm1
@@ -1,4 +1,8 @@
 function create {
     write-host "from devops through vs code "
+    write-host "from devops through vs code "
+    write-host "from devops through vs code "
+    write-host "from devops through vs code "
+    write-host "from devops through vs code "
 }
     
